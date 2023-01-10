b5783fd50800ffb11beb15648ee70596f7bacefc
cyborgbuilder <isuruud8@gmail.com>
2023-01-10 20:16:30 +0530

    Page basic Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Page from './components/Page';
 import VideoPage from './components/VideoPage';
 
 function App() {
   return (
     <div className="App">
-      <VideoPage />
+      <Page />
     </div>
   );
 }
