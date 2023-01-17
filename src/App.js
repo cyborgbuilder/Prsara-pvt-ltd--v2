eb6b13a5f9e7e698a9b2b0855b0f11e460a1aaf0
cyborgbuilder <isuruud8@gmail.com>
2023-01-17 09:56:39 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
 import Page from './components/Page';
 import VideoPage from './components/VideoPage';
 
 function App() {
+  let component
+   // eslint-disable-next-line default-case
+   switch (window.location.pathname) {
+     case "/":
+       component = <Home />
+       break;
+     case "/about":
+       component = <VideoPage />
+       break;
+     case "/page":
+       component = <Page />
+       break;
+   }
   return (
     <div className="App">
-      <Page />
+      <Navbar />
+      {component}
     </div>
   );
 }
